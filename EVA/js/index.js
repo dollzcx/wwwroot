d2308b6d2ddab9e1d7d335398ae53926cec306d5
dollzcx <dollzcx@qq.com>
2017-05-01 21:12:28 +0800

    上传响应式页面EVA

---
+++ EVA/js/index.js
@@ -0,0 +1,11 @@
+$('#slide1').click(function(event) {
+  $('#EVA-carousel').carousel(0);
+});
+$('#slide2').click(function(event) {
+  $('#EVA-carousel').carousel(1);
+});
+$('#slide3').click(function(event) {
+  $('#EVA-carousel').carousel(2);
+});
+
+
